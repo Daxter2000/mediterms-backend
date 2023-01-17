ddbf7b2dde234f2f41a210e3e9300777f2fcc2d1
= <=omar.murillo@inteligene.com>
2023-01-17 11:59:54 -0700

    feat: Migrations to add foreign keys to tables

---
+++ migrations/20230117181316-add_term_definiton_id_to_term_tables.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.addColumn(
+      'terms', // name of Source model
+      'defintionId', // name of the key we're adding 
+      {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'terms_definitions', // name of Target model
+          key: 'id', 
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      }
+    );
+  },
+  async down (queryInterface, Sequelize) {
+    await queryInterface.removeColumn(
+      'terms', // name of Source model
+      'definitionId' // key we want to remove
+    );
+  }
+};
