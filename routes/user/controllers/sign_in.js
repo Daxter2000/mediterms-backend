37cfa5ef0f70e3682f5c08becba11f30d1e0ee5d
= <=omar.murillo@inteligene.com>
2023-01-18 16:51:17 -0700

    feat: Controllers for signing in

---
+++ routes/user/controllers/sign_in.js
@@ -0,0 +1,40 @@
+const {handleCommonErrors, } = require("#utils/controllers/index")
+
+const signIn =  async function(request, reply, fastify){
+    const User = fastify.db.user
+
+    const {email, password} = request.body
+    try {
+        const userRecord = await User.findOne({
+            where: {email: email},
+            raw: true
+        })
+        
+        if(userRecord.length === 0) return reply.status(400).send({code: "MDT_APP_USER_NOT_FOUND", errors: ["El usuario o la contraseña son incorrectas "]})
+
+        //MATCH PASSWORD
+        const matchPassword = await fastify.bcrypt.compare(password, userRecord.password)
+        if (!matchPassword) reply.status(400).send({ errors: ['El usuario o la contraseña son incorrectas ' ], code: 'MDT_APP_PASSWORD_BAD_SUPPLIED' })
+
+        // CREATING TOKEN
+        const token = fastify.jwt.sign({ userId: userRecord.id }, { expiresIn: '1 year' }) 
+       
+        //SAVE THE TOKEN IN DATABASE
+        await User.update({
+            token: token
+        }, {
+           where: { id: userRecord.id}
+        })
+        // console.log(userRecord.email)
+        // const userRow = await User.findOne({where: {email: userRecord.email}})
+        // console.log(userRow)
+        // userRow.token = token
+        // await userRow.save();
+
+        return reply.send( {token, user: {name: userRecord.name, email: userRecord.email}})
+    } catch (e) {
+        reply.status(400).send({ ...handleCommonErrors(e) })
+    }
+}
+
+module.exports = {signIn}
