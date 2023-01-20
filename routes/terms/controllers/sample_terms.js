528ac603cd88b55fb978d100ba87f51722abbe4c
= <=omar.murillo@inteligene.com>
2023-01-20 11:54:02 -0700

    fix: Update controller for sampling terms

--- routes/terms/controllers/sample_terms.js
+++ routes/terms/controllers/sample_terms.js
@@ -1,24 +1,35 @@
 const {handleCommonErrors, } = require("#utils/controllers/index")
-
+const {Op, where} = require("sequelize")
 
 const fetchSampleTerms =  async function(request, reply, fastify){
     const SampleTerm = fastify.db.sample_term
     const TermsDefinition = fastify.db.terms_definition
 
+    const {previousIds} = request.query
+    let whereClause = {}
     try {
-        let sampleTermsJson = []
+        let sampleTermsJson = {}
 
+        if(previousIds != undefined && previousIds != null)
+            whereClause["id"] = {[Op.notIn]: JSON.parse(previousIds)}
+        
         const sampleTermsRecords = await SampleTerm.findAll({
           include: {
             model: TermsDefinition
           },
-          attributes: ["term", "definition_1", "definition_2"],
+          where: {
+            ...whereClause
+          },
+          order: [
+            ['id', 'DESC'],
+          ],
+          attributes: ["id","term", "definition_1", "definition_2"],
           raw: true,
           nest: true,
         })
 
         if(sampleTermsRecords.length === 0)
-          return reply.status(400).send({ code: "MDT_DB_OUT_BOUNDARIES", errors: ["No existen datos que mostrar"]})
+          return reply.status(400).send({ code: "MDT_DB_OUT_BOUNDARIES", errors: ["Has llegado el limite de muestra de terminos"]})
 
         sampleTermsRecords.map((termRecord)=>{
           const termDefinitions = [
@@ -28,10 +39,11 @@ const fetchSampleTerms =  async function(request, reply, fastify){
           ]
           const termDefinitionsShuffled = termDefinitions.sort((a, b) => 0.5 - Math.random());
           
-          sampleTermsJson.push({
+          sampleTermsJson = {
+            id: termRecord.id,
             term: termRecord.term,
             definitions: termDefinitionsShuffled
-          })
+          }
         })
   
         return reply.send(sampleTermsJson)
