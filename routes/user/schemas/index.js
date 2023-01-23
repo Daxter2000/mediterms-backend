90ba37d4a347791c7ea7744306098db573014972
= <=omar.murillo@inteligene.com>
2023-01-23 16:19:15 -0700

    fix: Export emailschema

--- routes/user/schemas/index.js
+++ routes/user/schemas/index.js
@@ -2,5 +2,6 @@
 const {signUpSchema} = require("./sign_up")
 const {signInSchema} = require("./sign_in")
 const {changePasswordSchema, resetPasswordSchema, saveNewPasswordSchema} = require("./password")
+const {validateEmailSchema} = require("./email")
 
-module.exports = {signUpSchema, signInSchema, changePasswordSchema, resetPasswordSchema, saveNewPasswordSchema}
+module.exports = {signUpSchema, signInSchema, changePasswordSchema, resetPasswordSchema, saveNewPasswordSchema, validateEmailSchema}
